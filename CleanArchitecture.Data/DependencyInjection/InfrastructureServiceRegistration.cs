3e19a51c8fd36b9468eb6ab5ab3792548cef757a
Jose Wigner <jwlliully@fassil.com.bo>
2022-03-06 17:17:25 -0400

    integration email service

---
+++ CleanArchitecture.Data/DependencyInjection/InfrastructureServiceRegistration.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchitecture.Aplication.Contracts.Infrastructure;
+using CleanArchitecture.Aplication.Contracts.Persistence;
+using CleanArchitecture.Aplication.Models;
+using CleanArchitecture.Infrastructure.Email;
+using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.DependencyInjection
+{
+    public static class InfrastructureServiceRegistration
+    {
+
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<StreamerDbContext>(option =>
+             option.UseSqlServer(configuration.GetConnectionString("ConnectionString"))
+             );
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IVideoRepository, VideoRepository>();
+            services.AddScoped<IStreamerRepository, StreamerRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+
+    }
+}
---
+++ CleanArchitecture.Data/Email/EmailService.cs
@@ -0,0 +1,53 @@
+﻿using CleanArchitecture.Aplication.Contracts.Infrastructure;
+using CleanArchitecture.Aplication.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Email
+{
+    public class EmailService : IEmailService
+    {
+
+        public EmailSettings _emailSettings;
+        public ILogger<EmailService> _logger;
+
+        public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
+        {
+            _emailSettings = emailSettings.Value;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendEmail(Aplication.Models.Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var form = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(form, to, subject, emailBody, emailBody);
+
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            if (response.StatusCode.Equals(HttpStatusCode.Accepted) || response.StatusCode.Equals(HttpStatusCode.OK))
+                return true;
+
+            _logger.LogError("No se pudo enviar el correo");
+            return false;
+        }
+    }
+}
